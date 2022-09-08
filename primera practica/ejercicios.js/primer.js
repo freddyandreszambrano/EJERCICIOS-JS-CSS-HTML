04dbf1a10b12589f7212f34f9ac919aab30a8fc0
freddy andres <108620989+freddyandreszambrano@users.noreply.github.com>
2022-09-07 22:39:27 -0500

    Add files via upload

---
+++ primera practica/ejercicios.js/primer.js
@@ -0,0 +1,17 @@
+function numsuma(){
+    let num1=6
+    let num2=10
+    console.log(num1 + num2)
+}
+console.log('este es un mensaje que solo se ve en consola ')
+
+
+function suma(a,b){return a+b}
+
+let lista=[1,2,3,4,5,6]
+let lim= length(lista)
+for(elemento of lista_){
+    let suma = suma(elemento,5)
+    console.log(suma)
+}
+
